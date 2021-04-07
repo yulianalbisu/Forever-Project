336b153e5b456194cf26a47419481ed1679bf233
Yuliana <ya.aldrich@yahoo.com>
2021-04-07 21:21:50 +0000

    user1 and user2 added

--- model.py
+++ model.py
@@ -33,15 +33,16 @@ class Link(db.Model):
 
     link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user1_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False) #user1 should connect by link to user2
-    user2_id = db.Column(db.Integer)
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     date_rel = db.Column(db.DateTime) #should be a date
 
     
-    users = db.relationship('User')
+    user_1 = db.relationship('User', foreign_keys=[user1_id])
+    user_2 = db.relationship('User', foreign_keys=[user2_id])
     answers = db.relationship('Answer')
 
     def __repr__(self):
-        return f'<Link link_id={self.link_id} user2_id={self.user2_id} date_rel={self.date_rel}>'
+        return f'<Link link_id={self.link_id} name2={self.name2} date_rel={self.date_rel}>'
 
 class Question(db.Model):
     """Form for user to answer"""
