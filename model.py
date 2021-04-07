1665b5bd49f3db43b2cfca14c8fc37ef6baa121c
Yuliana <ya.aldrich@yahoo.com>
2021-04-07 00:09:42 +0000

    Classes and relationships define

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-class Users(db.Model):
+class User(db.Model):
     """The users data"""
 
     __tablename__ = 'users'
@@ -18,19 +18,19 @@ class Users(db.Model):
     phone = db.Column(db.Integer, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     link_id = db.Column(db.Integer,
-                        db.ForeignKey('link.link_id'),
+                        db.ForeignKey('links.link_id'),
                         nullable=False)
     
 
     link = db.relationship('Link')
-    answers = db.relationship('Answers') ##this relationship is with my primary key, connected by name?
+    answers = db.relationship('Answer') ##this relationship is with my primary key, connected by name?
 
-    def __repr__(self):
+    #def __repr__(self):
 
 class Link(db.Model):
     """Link a code to connect couple"""
 
-    __tablename__ = 'link'
+    __tablename__ = 'links'
 
     link_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user1_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False) #will get personal info?
@@ -38,12 +38,12 @@ class Link(db.Model):
     date_rel = db.Column(db.DateTime) #should be a date
 
     
-    users = db.relationship('Users')
-    answers = db.relationship('Answers')
+    users = db.relationship('User')
+    answers = db.relationship('Answer')
 
-    def __repr__(self):
+    #def __repr__(self):
 
-class Questions(db.Model):
+class Question(db.Model):
     """Form for user to answer"""
 
     __tablename__ = 'questions'
@@ -51,24 +51,41 @@ class Questions(db.Model):
     question_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     question = db.Column(db.String)
     
-    answers = db.relationship('Answers')
+    answers = db.relationship('Answer')
 
-    def __repr__(self):
+    #def __repr__(self):
 
 
-class Answers(db.Model):
+class Answer(db.Model):
     """Answer from question's form"""
 
     __tablename__ = 'answers'
 
     answer_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'))
-    answer1 = db.Column(db.String, db.ForeignKey('link.user1_id')) ## 2 items, same foreign key
-    answer2 = db.Column(db.String, db.ForeignKey('link.user2_id'))
+    answer = db.Column(db.String ) ## 2 items, same foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
-questions = db.relationship('Questions')
-link = db.relationship('Link') ## should I create 2 relationship since same table?
-users = db.relationship('Users')
+    questions = db.relationship('Question')
+    users = db.relationship('User')
 
-def __repr__(self):
+    #def __repr__(self):
+
+def connect_to_db(flask_app, db_uri='postgresql:///my_forever', echo=False):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app, echo=False)
