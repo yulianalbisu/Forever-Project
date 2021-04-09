87d64ac8c0a4d5b73693ecc18c1b6167ec00a11b
Yuliana <ya.aldrich@yahoo.com>
2021-04-09 17:54:52 +0000

    seed.py review

--- seed.py
+++ seed.py
@@ -14,7 +14,6 @@ import server
 
 os.system('dropdb forever')
 os.system('createdb forever')
-
 model.connect_to_db(server.app)
 model.db.create_all()
 
@@ -23,26 +22,39 @@ with open('data/questions.json') as f:
     question_data = json.loads(f.read())
 
 
-    questions_in_db = []
-    for question in question_data:
-        question = (question['question'])
+questions_in_db = []
+for question in question_data:
+    question = (question['question'])
 
-        db_question = crud.create_question(question, answers)
+    db_question = crud.create_question(question, answers)
         
-        questions_in_db.append(db_question)
-
-    
-
+    questions_in_db.append(db_question)
 
 
    # user = User.query.options(db.joinedload('links')).all())
+for n in range(10):
+    email = f'user{n}@test.com'
+    password = 'test'
 
-    #for n in range(10):
-        #email = f'user{n}@test.com'
-        #password = 'test'
+    user = crud.create_user(email,password)
 
-        #user = crud.create_user(email, password, name, gender)
+    #creating a link to get 2 users synchronized 
+    for m in range(10):
+        link = {m}
+        user_id1 = f'{m}email, password'
+        user_id2 = f'{m}email, password'
 
+        link = crud.create_link(link, user_id1, user_id2)
+
+    #creating an answer from users
+    for _ in range(10):
+        rendom_question = choice(questions_in_db)
+        answer = randint(input["answer"]) #check if will work with input since user has to answer.
+
+        crud.create_answer(user, random_question, answer)
+
+    
+        
         
 
 
